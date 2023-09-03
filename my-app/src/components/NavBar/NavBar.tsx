c23b1ee3884ceb4332243de523d8524c0c6a9708
yoqubovich2001 <workibaydullayev@gmail.com>
2023-09-03 23:29:10 +0500

    Edited navbar folder and improved

--- my-app/src/components/NavBar/NavBar.tsx
+++ my-app/src/components/NavBar/NavBar.tsx
@@ -1,22 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 import styles from "./Navbar.module.scss";
 import { Link } from "react-scroll";
-import {NavLink} from "react-router-dom"
 import logo from "../../images/logo.png";
-import classNames from "classnames";
+import classNames from 'classnames'
 export const NavBar: React.FC = () => {
+  
+  useEffect(() => {
+    controlActive()
+  },[])
+  const controlActive = () => {
+    const navbar =document.querySelectorAll('button')
+    navbar.forEach(element => {
+      element.addEventListener('click', function () {
+        navbar.forEach(nav => nav.classList.remove(styles.active))
+        
+        this.classList.add(styles.active)
+      })
+    })
+  }
   return (
     <div className={styles.navbar}>
       <img className={styles.logo} src={logo} alt="" />
       <ul className={styles.navbarList}>
         <li>
           <Link to="main" spy={true} smooth={true} offset={50} duration={500}>
-            <button className={styles.navbarBtn}>Home</button>
+            <button className={classNames(styles.navbarBtn, styles.active)} onClick={controlActive}>
+              Home
+            </button>
           </Link>
         </li>
         <li>
           <Link to="about" spy={true} smooth={true} offset={50} duration={500}>
-            <button className={styles.navbarBtn}>About</button>
+            <button
+              className={styles.navbarBtn}
+              onClick={controlActive}
+            >
+              About
+            </button>
           </Link>
         </li>
         <li>
@@ -27,11 +48,11 @@ export const NavBar: React.FC = () => {
             offset={50}
             duration={500}
           >
-            <button className={styles.navbarBtn}>Portfolio</button>
+            <button className={styles.navbarBtn} onClick={controlActive}>Portfolio</button>
           </Link>
         </li>
         <li>
-          <button className={styles.navbarBtn}>Resume</button>
+          <button className={styles.navbarBtn} onClick={controlActive}>Resume</button>
         </li>
         <li>
           <Link
@@ -41,7 +62,7 @@ export const NavBar: React.FC = () => {
             offset={50}
             duration={500}
           >
-            <button className={styles.navbarBtn}>Contact</button>
+            <button className={styles.navbarBtn} onClick={controlActive}>Contact</button>
           </Link>
         </li>
       </ul>
